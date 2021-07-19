d8612f7aa28fda5a5f5cecfa62a5ef83fa6e4419
Brian Dady <bndady@gmail.com>
2021-07-19 10:19:52 -0500

    Moves uninstall logic to uninstall.php and makes it actually work

--- uninstall.php
+++ uninstall.php
@@ -6,11 +6,10 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 
 require plugin_dir_path( __FILE__ ) . 'includes/class-better-banners.php';
 
-function better_banners_uninstall() {
-    global $wpdb;
 
-    $table_name = $wpdb->prefix . 'posts';
-    $post_type = TotallyQuiche\BetterBanners\Better_Banners::$custom_post_type_slug;
+global $wpdb;
 
-    $wpdb->query("DELETE FROM `$table_name` WHERE `post_type` = $post_type;");
-}
+$table_name = $wpdb->prefix . 'posts';
+$post_type = TotallyQuiche\BetterBanners\Better_Banners::$custom_post_type_slug;
+
+$wpdb->query("DELETE FROM `$table_name` WHERE `post_type` = \"$post_type\";");
