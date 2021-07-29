047e1bce0c50b913d862f6ca6c21e4e861d43852
Brian Dady <bndady@gmail.com>
2021-07-29 17:40:55 -0500

    Minor refactor to autoloader for readability

--- includes/class-autoloader.php
+++ includes/class-autoloader.php
@@ -14,19 +14,28 @@ final class Autoloader {
         $plugin_dir_path = plugin_dir_path( __FILE__ );
 
         $class_map = array(
-            'TotallyQuiche\BetterBanners\Better_Banners'                               => $plugin_dir_path . 'class-better-banners.php',
-            'TotallyQuiche\BetterBanners\ActionHandlers\Action_Handler'                => $plugin_dir_path . 'action_handlers/interface-action-handler.php',
-			'TotallyQuiche\BetterBanners\ActionHandlers\Init_Handler'                  => $plugin_dir_path . 'action_handlers/class-init-handler.php',
-			'TotallyQuiche\BetterBanners\ActionHandlers\Admin_Enqueue_Scripts_Handler' => $plugin_dir_path . 'action_handlers/class-admin-enqueue-scripts-handler.php',
-			'TotallyQuiche\BetterBanners\ActionHandlers\Wp_Enqueue_Scripts_Handler'    => $plugin_dir_path . 'action_handlers/class-wp-enqueue-scripts-handler.php',
-			'TotallyQuiche\BetterBanners\ActionHandlers\Admin_Menu_Handler'            => $plugin_dir_path . 'action_handlers/class-admin-menu-handler.php',
-			'TotallyQuiche\BetterBanners\ActionHandlers\Wp_Body_Open_Handler'          => $plugin_dir_path . 'action_handlers/class-wp-body-open-handler.php',
-			'TotallyQuiche\BetterBanners\ActionHandlers\Add_Meta_Boxes_Handler'        => $plugin_dir_path . 'action_handlers/class-add-meta-boxes-handler.php',
-			'TotallyQuiche\BetterBanners\ActionHandlers\In_Admin_Footer_Handler'       => $plugin_dir_path . 'action_handlers/class-in-admin-footer-handler.php',
+            'TotallyQuiche\BetterBanners\Better_Banners'
+                => 'class-better-banners.php',
+            'TotallyQuiche\BetterBanners\ActionHandlers\Action_Handler'
+                => 'action_handlers/interface-action-handler.php',
+			'TotallyQuiche\BetterBanners\ActionHandlers\Init_Handler'
+                => 'action_handlers/class-init-handler.php',
+			'TotallyQuiche\BetterBanners\ActionHandlers\Admin_Enqueue_Scripts_Handler'
+                => 'action_handlers/class-admin-enqueue-scripts-handler.php',
+			'TotallyQuiche\BetterBanners\ActionHandlers\Wp_Enqueue_Scripts_Handler'
+                => 'action_handlers/class-wp-enqueue-scripts-handler.php',
+			'TotallyQuiche\BetterBanners\ActionHandlers\Admin_Menu_Handler'
+                => 'action_handlers/class-admin-menu-handler.php',
+			'TotallyQuiche\BetterBanners\ActionHandlers\Wp_Body_Open_Handler'
+                => 'action_handlers/class-wp-body-open-handler.php',
+			'TotallyQuiche\BetterBanners\ActionHandlers\Add_Meta_Boxes_Handler'
+                => 'action_handlers/class-add-meta-boxes-handler.php',
+			'TotallyQuiche\BetterBanners\ActionHandlers\In_Admin_Footer_Handler'
+                => 'action_handlers/class-in-admin-footer-handler.php',
         );
 
         if ( array_key_exists( $class_name, $class_map ) ) {
-            require_once $class_map[ $class_name ];
+            require_once plugin_dir_path( __FILE__ ) . $class_map[ $class_name ];
         }
     }
 }
