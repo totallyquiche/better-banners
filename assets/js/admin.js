bfe492a99a3aa9d8fa698bcf2648a0b6cd1018c7
Brian Dady <bndady@gmail.com>
2021-08-10 21:01:24 -0500

    Fixes color picker script to allow for saving new color properly

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,23 +1,6 @@
 ( ( $ ) => {
-	const initializeEditor = function () {
-		if (typeof tinyMCE.activeEditor.dom !== 'undefined') {
-			tinyMCE
-				.activeEditor
-				.dom
-				.setStyle(
-					tinymce.activeEditor.dom.select('body'),
-					'background-color',
-					$('#tqbb01-background-color').val()
-				);
-
-			clearInterval(initializer);
-		}
-	};
-
-	const initializer = setInterval(initializeEditor, 100);
-
 	$(document).on('ready', function () {
-		$('#publish').append('<input type="hidden" name="tqbb01-background-color" value="" />');
+		$('#publish').append('<input type="hidden" name="tqbb01-background-color" />');
 
 		$('#tqbb01-background-color').wpColorPicker({
 			palettes: true,
@@ -28,5 +11,27 @@
 				setTimeout(initializeEditor, 10);
 			}
 		});
+
+		const initializeEditor = function () {
+			if (
+				typeof tinyMCE.activeEditor.dom !== 'undefined' &&
+				$('#tqbb01-background-color').val()
+			) {
+				tinyMCE
+					.activeEditor
+					.dom
+					.setStyle(
+						tinymce.activeEditor.dom.select('body'),
+						'background-color',
+						$('#tqbb01-background-color').val()
+					);
+
+				$('input[name="tqbb01-background-color"]').val($('#tqbb01-background-color').val());
+
+				clearInterval(initializer);
+			}
+		};
+
+		const initializer = setInterval(initializeEditor, 100);
 	});
 } )( jQuery );
